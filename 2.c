00af447072f5bd367386f51b18ba4c04829b2d97
san4ouzxc <120376962+san4ouzxc@users.noreply.github.com>
2025-03-06 22:54:20 +0200

    Add files via upload

--- 2.c
+++ 2.c
@@ -1,3 +1,32 @@
+/// 1
+#include <stdio.h>
+
+int main() {
+   printf("Hello, World!\n");
+   return 0;
+}
+
+/// 2
+#include <stdio.h>
+
+int arr[1000];  
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+/// 3
+#include <stdio.h>
+
+int arr[1000] = {1}; 
+
+int main() {
+   printf("Hello, World!\n");
+   return 0;
+}
+
+/// 4
 #include <stdio.h>
 
 void func() {
