4c26200a625857cab3031d34bd7ce5335b86bc74
san4ouzxc <120376962+san4ouzxc@users.noreply.github.com>
2025-03-06 22:49:10 +0200

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,21 +1,51 @@
+/// для 32-бітної системи
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>  
+
+void check_time_t_overflow() {
+    time_t current_time = time(NULL);
+
+    printf("Поточний час: %ld\n", current_time);
+}
 
 int main() {
-    time_t current_time;
-    
-    current_time = time(NULL);
-    printf("Поточний час (epoch): %ld\n", current_time);
+    time_t mock_time = 2147483644;  
 
-    #if defined(__x86_64__) || defined(__aarch64__)
-        // 64-бітна архітектура
-        printf("Для 64-бітної архітектури time_t не переповниться на людському рівні.\n");
-    #else
-        // 32-бітна архітектура
-        time_t max_32bit_time = 2147483647;  // Максимум для 32-бітної архітектури
-        printf("Максимальний час для 32-бітної архітектури: %ld секунд\n", max_32bit_time);
-        printf("Це відбудеться 19 січня 2038 року.\n");
-    #endif
+    printf("Початковий час (epoch): %ld\n", mock_time);
+
+    for (int i = 0; i < 5; i++) {
+        mock_time++; 
+        printf("Час через %d секунд: %ld\n", i + 1, mock_time);
+        sleep(1);  
+    }
 
     return 0;
 }
+
+/// для 64-бітної системи
+
+/* #include <stdio.h>
+#include <time.h>
+#include <unistd.h>  
+
+void check_time_t_overflow() {
+    time_t current_time = time(NULL);
+
+    printf("Поточний час: %ld\n", current_time);
+}
+
+int main() {
+    time_t mock_time = 9223372036854775804;  
+
+    printf("Початковий час (epoch): %ld\n", mock_time);
+
+    for (int i = 0; i < 5; i++) {
+        mock_time++; 
+        printf("Час через %d секунд: %ld\n", i + 1, mock_time);
+        sleep(1);  
+    }
+
+    return 0;
+} */
+
